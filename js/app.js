f500279b3a54198aa703ec0e99280ae673ca1adf
Allen Zhong <zhong.allen@gmail.com>
2016-01-16 17:28:30 +0800

    add scrollmagic

--- js/app.js
+++ js/app.js
@@ -1,29 +1,35 @@
-
-function animateLogo(){
-  TweenMax.fromTo('.react-logo',5,{
-    css:{
+function animateLogo() {
+  TweenMax.fromTo('.react-logo', 5, {
+    css: {
       y: "-50px",
     }
-  },{
-    css:{
+  }, {
+    css: {
       y: "30px",
     },
     repeat: -1,
     yoyo: true,
     ease: Power2.easeInOut
-  }
-                 );
-}
-function animateRobot(){
-  var t = new TimelineMax({yoyo: true, repeat: -1});
-  t.to("#android-robot", 1, {rotation: "-30deg"})
-  .to("#android-robot", 1, {rotation:'-55deg'});
+  });
 }
 
-function updateSliderControl(){
+function animateRobot() {
+  var t = new TimelineMax({
+    yoyo: true,
+    repeat: -1
+  });
+  t.to("#android-robot", 1, {
+      rotation: "-30deg"
+    })
+    .to("#android-robot", 1, {
+      rotation: '-55deg'
+    });
+}
+
+function updateSliderControl() {
   var links = document.querySelectorAll("#slider-control a");
 
-  for(var i = 0; i < links.length; i++){
+  for (var i = 0; i < links.length; i++) {
     var link = links[i];
 
     var sectionName = link.getAttribute('href');
@@ -33,7 +39,7 @@ function updateSliderControl(){
 
     //alert('sectionTop '+ sectionTop);
     // Check if window.scrollY is between the section.
-    if(window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
+    if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
       link.className = "active";
     } else {
       link.className = "";
@@ -41,13 +47,13 @@ function updateSliderControl(){
   }
 }
 
-function addSmoothScrolling(){
+function addSmoothScrolling() {
   var links = document.querySelectorAll("#slider-control a");
 
-  for(var i = 0; i < links.length; i++){
+  for (var i = 0; i < links.length; i++) {
     var link = links[i];
-    (function(element){
-        element.addEventListener("click",function(event) {
+    (function(element) {
+      element.addEventListener("click", function(event) {
         // `event` is the mouse click event
         var sectionName = element.getAttribute('href');
         var section = document.querySelector(sectionName);
@@ -59,23 +65,54 @@ function addSmoothScrolling(){
   }
 }
 
-function scrollToElement(element){
+function scrollToElement(element) {
   var topOfElement = element;
-  TweenMax.to(window,1,{
-    scrollTo:{
+  TweenMax.to(window, 1, {
+    scrollTo: {
       y: topOfElement.offsetTop,
     },
     ease: Power2.easeInOut
   });
 }
 
-window.onload = function(){
+function addScrollBling() {
+  var controller = new ScrollMagic.Controller();
+  var scene1 = new ScrollMagic.Scene({
+      triggerElement: "#native",
+      triggerHook: "onEnter",
+      duration: "100%"
+    }).addTo(controller)
+    .setTween("#intro-section .fade-overlay", 1, {
+      opacity: 1
+    });
+
+  var scene2 = new ScrollMagic.Scene({
+      triggerElement: "#intro-section",
+      triggerHook: "onLeave",
+      duration: "100%"
+    }).addTo(controller)
+    .setTween("#iphone-overlay", 1, {
+      y: 0,
+      width: "50%"
+    });
+
+  var scene3 = new ScrollMagic.Scene({
+    triggerElement: "#native",
+    triggerHook: "onLeave",
+    duration: "100%"
+  }).addTo(controller)
+    .setPin("#iphone-overlay");
+}
+
+
+window.onload = function() {
   animateLogo();
   animateRobot();
   updateSliderControl();
   addSmoothScrolling();
+  addScrollBling();
 };
 
-window.onscroll = function(){
+window.onscroll = function() {
   updateSliderControl();
 }
